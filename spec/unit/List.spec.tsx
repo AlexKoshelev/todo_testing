d884eec02df371151c3dd1195106fb7e71c55359
Alexandr Koshelev <oit@uf-vsuwt.ru>
2024-02-17 22:47:48 +0500

    feat: проверил, что мы не можем ввести более 10 невыполненных задачь

--- spec/unit/List.spec.tsx
+++ spec/unit/List.spec.tsx
@@ -1,7 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { prettyDOM, render, screen } from "@testing-library/react";
+import { App } from "src/App";
 import { List } from "src/components/List";
+import { JestStoreProvider } from "../utils/JestStoreProvider";
+import ue from "@testing-library/user-event";
 
-it("отображение списка задач", () => {
+it.skip("отображение списка задач", () => {
   const onDelete = jest.fn();
   const onToggle = jest.fn();
 
@@ -27,7 +30,7 @@ it("отображение списка задач", () => {
     <List items={items} onDelete={onDelete} onToggle={onToggle} />
   );
   const firstRender = asFragment();
-  
+
   items.pop();
 
   rerender(<List items={items} onDelete={onDelete} onToggle={onToggle} />);
@@ -36,6 +39,21 @@ it("отображение списка задач", () => {
   expect(firstRender).toMatchDiffSnapshot(secondRender);
 });
 
-it("Список содержит не больше 10 невыполненных задач", () => {
+it("Список содержит не больше 10 невыполненных задач", async () => {
+  const userEvent = ue.setup({
+    advanceTimers: jest.advanceTimersByTime,
+  });
 
+  render(<App />, { wrapper: JestStoreProvider });
+
+  const input = screen.getByTestId("input-field");
+
+  const addBtn = screen.getByAltText(/Добавить/i);
+
+  for (let i = 1; i <= 12; i++) {
+    await userEvent.type(input, `task ${i}`);
+    await userEvent.click(addBtn);
+  }
+  const tasks = screen.queryAllByRole("checkbox");
+  expect(tasks.length).toEqual(10);
 });
